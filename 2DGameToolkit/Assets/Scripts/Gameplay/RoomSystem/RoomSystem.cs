a512de340b49e731682a33887fecd46102dffb49
AdrienMarcenat <adrien.marcenat@gmail.com>
2019-04-27 11:27:18 +0200

    fix canvas displayed and ialogue layout

---
+++ 2DGameToolkit/Assets/Scripts/Gameplay/RoomSystem/RoomSystem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class RoomSystem : MonoBehaviour
+{
+    public static Transform m_CurrentRoom;
+
+    [SerializeField] GameObject m_ExitRoom;
+    [SerializeField] GameObject m_EnterRoom;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            m_CurrentRoom = m_EnterRoom.transform;
+            m_EnterRoom.SetActive(true);
+            m_ExitRoom.SetActive(false);
+        }
+    }
+}
