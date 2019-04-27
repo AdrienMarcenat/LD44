db1dcecca8530f06e3331eb9ba2268979447d4b9
AdrienMarcenat <adrien.marcenat@gmail.com>
2019-04-27 20:19:06 +0200

    add looping path

--- 2DGameToolkit/Assets/Scripts/Gameplay/Enemy/EnemyPath.cs
+++ 2DGameToolkit/Assets/Scripts/Gameplay/Enemy/EnemyPath.cs
@@ -4,18 +4,27 @@ public class EnemyPath : Enemy
 {
     [SerializeField] private BezierCurve m_Path;
     [SerializeField] private float m_Duration;
+    [SerializeField] private bool m_Loop;
+    private int m_Direction = 1;
 
     private float m_Progress;
 
     private void Update ()
     {
-        m_Progress += Time.deltaTime / m_Duration;
-        if (m_Progress > 1f)
+        m_Progress += m_Direction* (Time.deltaTime / m_Duration);
+        if ((m_Direction == 1 && m_Progress > 1f) || (m_Direction == -1 && m_Progress < 0f))
         {
-            Destroy (gameObject);
+            if (m_Loop)
+            {
+                m_Direction *= -1;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
         }
         Vector2 position = m_Path.GetPoint (m_Progress);
-        transform.localPosition = position;
+        transform.position = position;
     }
 
     public void SetPath (BezierCurve path)
